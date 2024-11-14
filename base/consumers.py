f53f8beb3c88cb3b9ab5c7ae371440a2fbfc2f4f
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-11-14 13:37:17 +0000

    update: users chat has been implemented

--- base/consumers.py
+++ base/consumers.py
@@ -2,6 +2,8 @@ from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+from .models import Conversation, Message
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -19,14 +21,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
         
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        data = {
+            'conversation_id': text_data_json['conversation_id'],
+            'body': text_data_json['body']
+        }
+
+        message = await self.create_message(data)
+        
         await self.channel_layer.group_send(
             self.room_group_name, {
             'type': 'chat_message', 
-            'body': text_data_json['body']
+            'message': message,
+
         })
+
+    @sync_to_async
+    def create_message(self, data):
+        conversation = Conversation.objects.get(
+            id=data['conversation_id'])
+        convo_other_user, created = Conversation.objects.get_or_create(
+            user=conversation.conversation_with,
+            conversation_with=conversation.user,
+        )
+        message = Message.objects.create(
+            conversation=conversation,
+            sender=conversation.user,
+            recipient=conversation.conversation_with,
+            body=data['body'])
+        Message.objects.create(
+            conversation=convo_other_user,
+            sender=message.sender,
+            recipient=message.recipient,
+            body=data['body'])
+        return message
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
-            'type': 'chat',
-            'message': event['body'],
-        }));
+            'type': 'message',
+            'message': event['message'].body,
+            'sender_id': str(event['message'].sender.id),
+        }))
