86d39d9d9f9ad9997b1053bd6ffc384d754e50c5
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-11-28 12:27:52 +0100

    update: websocket is created after a user is clicked on

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,26 @@ scrollContainers.forEach(scrollContainer => {
             scrollContainer.classList.remove('scrolling')}, 500);
     });
 });
+
+
+// const startConvo = document.querySelector('.start-new-convo');
+// const userSearch = document.querySelector('.user-search');
+
+// startConvo.addEventListener('click', () => {
+//     userSearch.style.display = ' block';
+// });
+
+const contactNav = document.getElementById('contact-nav');
+const sections = document.querySelectorAll('.section');
+
+
+contactNav.addEventListener('click', () => {
+    const contactSection = document.querySelector('.contacts-section');
+    const selectedNav = document.querySelector('.selected');
+    selectedNav.classList.remove('selected');
+    contactNav.classList.add('selected');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+    contactSection.style.display = '';
+})
