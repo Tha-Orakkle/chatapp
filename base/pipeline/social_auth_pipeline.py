666f5b70119cc2c4b4ff36fbc843a058c9f7c288
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-01-11 23:46:42 +0100

    fix: update email on social authentication only when user obj does not have an email

--- base/pipeline/social_auth_pipeline.py
+++ base/pipeline/social_auth_pipeline.py
@@ -1,13 +1,11 @@
 """ Update user object with social authentication details """
-from social_core.backends.github import GithubOAuth2
 
 
 def update_user_pipeline(strategy, details, backend, user=None, *args, **kwargs):
     if user:
-        if isinstance(backend, GithubOAuth2):
-            if details.get('email') and not user.email:
-                user.email = details.get('email')
-                user.save()
+        if details.get('email') and not user.email:
+            user.email = details.get('email')
+            user.save()
         if not user.profile.full_name:
             first_name = details.get('first_name').strip() or ''
             last_name = details.get('last_name').strip() or ''
